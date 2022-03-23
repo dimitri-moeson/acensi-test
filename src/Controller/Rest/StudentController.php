34fce771aaf297a63c42b13c6083a21904bea4db
MOESON <d.moeson@gmail.com>
2022-03-23 11:55:52 +0100

    Rest controller

---
+++ src/Controller/Rest/StudentController.php
@@ -0,0 +1,49 @@
+<?php
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+/**
+ * Class StudentController
+ */
+class StudentController extends FOSRestController
+{
+    /**
+     * @var StudentRepository
+     */
+    private $StudentRepository;
+
+    /**
+     * StudentController constructor.
+     * @param StudentRepository $studentRepository
+     */
+    public function __construct(StudentRepository $studentRepository){
+        $this->StudentRepository = $studentRepository;
+    }
+
+    /**
+     * Retrieves an Student resource
+     * @Rest\Get("/student/{studentId}")
+     */
+    public function getStudent(int $studentId): View
+    {
+        $student = $this->StudentRepository->findById($studentId);
+
+        if (!$student) {
+            throw new EntityNotFoundException('Article with id '.$studentId.' does not exist!');
+        }
+
+        // In case our GET was a success we need to return a 200 HTTP OK response with the request object
+        return View::create($student, Response::HTTP_OK);
+    }
+
+    /**
+     * Retrieves a collection of Student resource
+     * @Rest\Get("/students")
+     */
+    public function getArticles(): View
+    {
+        $students = $this->StudentRepository->findAll();
+        // In case our GET was a success we need to return a 200 HTTP OK response with the collection of student object
+        return View::create($students, Response::HTTP_OK);
+    }
+}
--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -4,6 +4,9 @@ namespace App\Form {
 
     use App\Entity\Student;
     use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+    use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
     use Symfony\Component\Form\FormBuilderInterface;
     use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -20,9 +23,25 @@ namespace App\Form {
         public function buildForm(FormBuilderInterface $builder, array $options): void
         {
             $builder
-                ->add('FirstName')
-                ->add('LastName')
-                ->add('NumEtud')
+                ->add('FirstName', TextType::class, [
+                    'label' => 'F.Name',
+                    'attr' => [
+                        'placeholder' => 'F.Name',
+                    ],
+                    'row_attr' => [
+                        'class' => 'form-floating',
+                    ],
+                ])
+                ->add('LastName', TextType::class, [
+                    'label' => 'L.Name',
+                    'attr' => [
+                        'placeholder' => 'L.Name',
+                    ],
+                    'row_attr' => [
+                        'class' => 'form-floating',
+                    ],
+                ])
+                ->add('NumEtud', IntegerType::class)
                 ->add('Department', ChoiceType::class, [
                     'placeholder' => 'Choose an option',
                 ])
